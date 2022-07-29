b445949e5f23eb604f425343104d149aecc422c9
Jon <jo@gmail.com>
2022-07-28 20:48:43 -0600

    draft 6

--- src/extension.ts
+++ src/extension.ts
@@ -17,33 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 	//var workingTree = cvsSCM.createResourceGroup('working-tree','Changes');
 	const cvsSCM = new CvsSourceControl(context);
 
-	// var listener1 = function(event: vscode.FileCreateEvent)  {
-	// 	console.log('FileCreateEvent', event);
-	// 	const changedResources: vscode.SourceControlResourceState[] = [];
-	// 	for(let i = 0; i < event.files.length; i++) {
-	// 		const resourceState: vscode.SourceControlResourceState = {resourceUri: event.files[i]};
-	// 		changedResources.push(resourceState);
-	// 		//workingTree.resourceStates = changedResources;
-	// 		workingTree.resourceStates = workingTree.resourceStates.concat(changedResources);
-	// 		console.log(workingTree.resourceStates.push());
-	// 	}
-	//   };
-
-	// var listener2 = function(event: vscode.TextDocumentWillSaveEvent) {
-	// 	console.log(event.document.fileName);
-	// 	const changedResources: vscode.SourceControlResourceState[] = [];
-	// 	const resourceState: vscode.SourceControlResourceState = {resourceUri: vscode.Uri.parse(event.document.fileName)};
-	// 	changedResources.push(resourceState);
-	// 	workingTree.resourceStates = workingTree.resourceStates.concat(changedResources);
-	// 	//console.log(workingTree.resourceStates.push({resourceUri: vscode.Uri.parse(event.document.fileName)}));
-	// 	console.log(workingTree.resourceStates.length);
-	// };
-
-	  
-	// start listening
-	// var subscription1 = vscode.workspace.onDidCreateFiles(listener1, context.subscriptions);
-	// var subscription2 = vscode.workspace.onWillSaveTextDocument(listener2, context.subscriptions);
-
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
