66815c050122100048a807139651dcabcf1274be
Jon <jo@gmail.com>
2022-08-02 15:20:40 -0600

    rm added file from Entries

--- src/cvsSourceControl.ts
+++ src/cvsSourceControl.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { promises as fsPromises } from 'fs';
 import { CvsRepository } from './cvsRepository';
 import * as path from 'path';
 import { SourceFileState } from './sourceFile';
@@ -229,20 +230,91 @@ export class CvsSourceControl implements vscode.Disposable {
 
 	// can only do this if file was untracked
 	async undoAdd(uri: vscode.Uri): Promise<void>  {
-		const { exec } = require("child_process");
-		const result = await new Promise<void>((resolve, reject) => {
-			const cvsCmd = `cvs remove ${path.basename(uri.fsPath)}`;
-			console.log(cvsCmd);
-			exec(cvsCmd, {cwd: path.dirname(uri.fsPath)}, (error: any, stdout: string, stderr: any) => {
-				if (error) {
-					vscode.window.showErrorMessage("Error deleting file.");
-					reject(error);
-				} else {
-					resolve();
+		const fs = require('fs');
+
+		// remove temp CVS file (e.g. 'test.txt,t')
+		await fs.readdir(path.dirname(uri.fsPath) + '/CVS', (err: string, files: string[]) => {
+			if (err) {
+				throw err;
+			}
+		
+			files.forEach(file => {
+				if(file.includes(path.basename(uri.fsPath))) {
+					fsPromises.rm(path.dirname(uri.fsPath) + '/CVS/' + file);
+					console.log('remove file');
 				}
 			});
 		});
+
+		const entries = await this.readCvsEntries(path.dirname(uri.fsPath) + '/CVS/Entries');
+
+		let newEntries = '';
+		entries.split(/\r?\n/).forEach(element => {
+			if (element.includes(path.basename(uri.fsPath)) === false) {
+				newEntries = newEntries.concat(element + '\n');
+			}
+		});
+
+		//remove last new line char
+
+		console.log(newEntries);
+		await this.writeCvsEntries(path.dirname(uri.fsPath) + '/CVS/Entries.out', newEntries);		
+
+		 
+		await fsPromises.rename(path.dirname(uri.fsPath) + '/CVS/Entries', path.dirname(uri.fsPath) + '/CVS/Entries.bak');
+		await fsPromises.rename(path.dirname(uri.fsPath) + '/CVS/Entries.out', path.dirname(uri.fsPath) + '/CVS/Entries');		
 	}
+
+	async readCvsEntries(path: string): Promise<string> {
+		const fs = require('fs/promises');
+
+		let data = '';
+
+		try{
+			data = await fs.readFile(path, {encoding: 'utf-8'});
+			console.log(data);		
+		} catch(err: any) {
+			console.log(err);
+		}
+
+		return data;
+	}
+
+	async writeCvsEntries(path: string, data: string): Promise<void> {
+		const fs = require('fs/promises');
+
+		try{
+			await fs.writeFile(path, data);
+		} catch(err: any) {
+			console.log(err);
+		}
+	}
+
+	// async getEntries(uri: vscode.Uri): Promise<void> {	
+	// 	const fs = require('fs');
+	// 	const readline = require('readline');
+
+	// 	// update Entires to remove line with added file name included 
+	// 	const inStream = fs.createReadStream(path.dirname(uri.fsPath) + '/CVS/Entries');
+	// 	const outStream = fs.createWriteStream(path.dirname(uri.fsPath) + '/CVS/Entries.out');
+
+	// 	const rl = readline.createInterface({
+	// 	input: inStream,
+	// 	output: outStream,
+	// 	crlfDelay: Infinity
+	// 	});
+
+	// 	const it = rl[Symbol.asyncIterator]();
+	// 	while (it !== undefined)
+	// 	{
+	// 		const line = await it.next();
+	// 		if (line.includes(path.basename(uri.fsPath)) === false) {
+	// 			outStream.write(line + '\n');
+	// 			console.log(line);
+	// 		}
+	// 	}
+	// 	console.log('done');
+	// }
 
 
     dispose() {
--- src/extension.ts
+++ src/extension.ts
@@ -93,7 +93,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Delete FIle');
+		vscode.window.showInformationMessage('Undo Addition of file');
 	});
 
 
