0ded9fbb27e67e4cb840071fa44e4a67ee99d3da
Schwartzkers <113959252+Schwartzkers@users.noreply.github.com>
2022-08-18 11:18:25 -0600

    added gutter diffs

--- src/cvsDocumentContentProvider.ts
+++ src/cvsDocumentContentProvider.ts
@@ -1,9 +1,9 @@
-import { CancellationToken, ProviderResult, TextDocumentContentProvider, Event, Uri, EventEmitter, Disposable, window } from "vscode";
+import { CancellationToken, ProviderResult, TextDocumentContentProvider, Event, Uri, EventEmitter, Disposable, window, workspace, SourceControlResourceState } from "vscode";
 import { CvsFile, CvsRepository, CVS_SCHEME } from './cvsRepository';
 import * as path from 'path';
 
 /**
- * Provides the content of the JS Fiddle documents as fetched from the server i.e.  without the local edits.
+ * Provides the content of the CVS files per the server version i.e.  without the local edits.
  * This is used for the source control diff.
  */
 export class CvsDocumentContentProvider implements TextDocumentContentProvider, Disposable {
@@ -11,7 +11,6 @@ export class CvsDocumentContentProvider implements TextDocumentContentProvider, 
 	private sourceControlFiles = new Map<string, CvsFile>(); // this assumes each file is only opened once per workspace
 
 	get onDidChange(): Event<Uri> {
-        console.log('onDidChange');
 		return this._onDidChange.event;
 	}
 
@@ -19,21 +18,45 @@ export class CvsDocumentContentProvider implements TextDocumentContentProvider, 
 		this._onDidChange.dispose();
 	}
 
-	async updated(resource: CvsFile): Promise<void> {
-        resource.text = await this.getRepositoryRevision(resource.uri);
-		this.sourceControlFiles.set(path.basename(resource.uri.fsPath), resource);
-        console.log(Uri.parse(`${CVS_SCHEME}:${resource.uri.fsPath}`));
-		this._onDidChange.fire(Uri.parse(`${CVS_SCHEME}:${resource.uri.fsPath}`));
+	async updated(resourceStates: SourceControlResourceState[]): Promise<void> {
+		this.sourceControlFiles.clear();
+
+		console.log('resourceStates.length = ' + resourceStates.length);
+
+		if (resourceStates.length > 0) {
+			console.log('inside if');
+			for (let i:number=0; i < resourceStates.length; i++) {
+				console.log('inside if i=: ' + i);
+				let cvsFIle = new CvsFile(resourceStates[i].resourceUri);
+				cvsFIle.text = await this.getRepositoryRevision(resourceStates[i].resourceUri);
+
+				const relativePath = workspace.asRelativePath(resourceStates[i].resourceUri.fsPath);
+				this.sourceControlFiles.set(Uri.parse(`${CVS_SCHEME}:${relativePath}`).fsPath, cvsFIle);
+
+				console.log('added to content provider: ' + Uri.parse(`${CVS_SCHEME}:${relativePath}`));			
+
+				this._onDidChange.fire(Uri.parse(`${CVS_SCHEME}:${relativePath}`));
+			}
+		}
+
+		this.sourceControlFiles.forEach(element => {
+			console.log(element.uri);
+			console.log(element.text);
+		});
 	}
 
 	provideTextDocumentContent(uri: Uri, token: CancellationToken): ProviderResult<string> {
-        console.log('provideTextDocumentContent');
+        console.log('provideTextDocumentContent: ' + uri);
 
         if (token.isCancellationRequested) { return "Canceled"; }
 
-		const resource = this.sourceControlFiles.get(path.basename(uri.fsPath));
-		if (!resource) { return "Resource not found: " + uri.toString(); }
+		const resource = this.sourceControlFiles.get(uri.fsPath);
+		if (!resource) {
+			console.log('provideTextDocumentContent failed');
+			return "Resource not found: " + uri.toString();
+		}
 
+		console.log(resource.text);
 		return resource.text;
 	}
 
